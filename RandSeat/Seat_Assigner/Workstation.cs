79fc86fe4b707482b4d8daba03500499eecee72b
gnsop <gabriel.snop@ou.edu>
2019-11-26 16:06:23 -0600

    comment out workstation

--- RandSeat/Seat_Assigner/Workstation.cs
+++ RandSeat/Seat_Assigner/Workstation.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Seat_Assigner
 {
-    class Workstation
-    {
-        public int comp_num { get; set; }
-        public int row_num   {get; set;}
-    }
+    //class Workstation
+    //{
+    //    public int comp_num { get; set; }
+    //    public int row_num   {get; set;}
+    //}
 
 
 
