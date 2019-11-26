56dda6c79b573fe82f3fc27299b89481a878399e
gnsop <gabriel.snop@ou.edu>
2019-11-26 15:52:30 -0600

    added workstation class

---
+++ RandSeat/Seat_Assigner/Workstation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seat_Assigner
+{
+    class Workstation
+    {
+        public int comp_num { get; set; }
+        public int row_num   {get; set;}
+    }
+}
