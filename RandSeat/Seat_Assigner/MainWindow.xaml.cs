fd6761f7344f4b058b738f4a2577d77ae0dec69c
Grant Mendell <Steven.G.Mendell-1@ou.edu>
2019-12-01 19:47:36 -0600

    Still working

--- RandSeat/Seat_Assigner/MainWindow.xaml.cs
+++ RandSeat/Seat_Assigner/MainWindow.xaml.cs
@@ -28,7 +28,26 @@ namespace Seat_Assigner
 
         private void CBname_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            LBoutput.Items.Add(CBname.SelectedItem);
+            //LBoutput.Items.Add(CBname.SelectedItem);
+            //List<int> number = new List<int>();
+            //Workstation computer = new Workstation();
+
+
+            //int num = 0;
+
+            //for(int i = 0; i < 50; i++)
+            //{
+            //    Random rand = new Random();
+
+            //    do
+            //    {
+            //        num = rand.Next(1, 49);
+            //    } while (number.Contains(num));
+            //}
+
+
+
+            
         }
 
         private void BTNName_Click(object sender, RoutedEventArgs e)
@@ -41,19 +60,35 @@ namespace Seat_Assigner
                 CBname.Items.Add(l[0] + " " + l[1]);
             }
 
+            List<int> number = new List<int>();
+
+            int num = 0;
+
+            for (int i = 0; i < 50; i++)
+            {
+                Random rand = new Random();
+
+                do
+                {
+                    num = rand.Next(1, 49);
+                } while (number.Contains(num));
+            }
+
+
+
             //Generate a number between 1 and 50
-            Random rand = new Random();
-            int nums = rand.Next(1, 50);
-            txtNumbers.Text = nums.ToString();
+            //Random rand = new Random();
+            //int nums = rand.Next(1, 50);
+            //txtNumbers.Text = nums.ToString();
         }
         
         //Returns number less than 50 
         //int num = random.Next(50);
-        public int RandomNumber(int min, int max)
-        {
-            Random rand = new Random();
-            return random.Next(min, max);
-        }
+        //public int RandomNumber(int min, int max)
+        //{
+        //    Random rand = new Random();
+        //    return rand.Next(min, max);
+        //}
 
         //int randomNumber;
         //Random rand = new Random();
@@ -61,12 +96,12 @@ namespace Seat_Assigner
         //Console.WriteLine("The random number is " + randomNumber)
         //    for(int i; int <= 50; int ++)
         //    {
-                
+
         //    }
         //    string text = System.IO.File.ReadAllText()
         //public class RandomGenerator
         //{
-        ////Generate a random number between two numbers
+        //Generate a random number between two numbers
         //    public int RandomNumber(int min, int max)
         //    {
         //        Random rand = new Random();
@@ -76,12 +111,47 @@ namespace Seat_Assigner
 
         private void LBoutput_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            LBoutput.Items.Add($"lName", "fName", "id");
+            LBoutput.Items.Add(CBname.SelectedItem);
+            List<int> number = new List<int>();
+            Workstation computer = new Workstation();
+
+
+            int num = 0;
+
+            for (int i = 0; i < 50; i++)
+            {
+                Random rand = new Random();
+
+                do
+                {
+                    num = rand.Next(1, 49);
+                } while (number.Contains(num));
+            }
+
+            Student student = new Student();
+            //Workstation computer = new Workstation();
+            //LBoutput.Items.Add($"{student.lName} {student.fName}: Seat {num}");
+            LBoutput.Items.Add($": Seat {num}");
         }
 
         //private void BtnClose_Click(object sender, RoutedEventArgs e)
         //{
         //    Close();
         //}
+
+        //public class Student
+        //{
+        //    private static Random rand;
+        //    private int[] numbers;
+        //    public Student()
+        //    {
+        //        rand = new Random();
+        //        numbers = new int[50];
+        //        for(int i = 0; i < 50; i++)
+        //        {
+        //            numbers[i] = rand.Next();
+        //        }
+        //    }
+        //} 
     }
 }
